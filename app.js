6075a1e1efc0ca845bf62a1e8fd5a270cd18435e
karmis12 <mjy1187@naver.com>
2023-05-15 10:21:03 +0900

    knowledge update

---
+++ app.js
@@ -0,0 +1,13 @@
+const a=4;
+const b=3;
+let myName="jimmy";
+
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("my name is "+myName);
+
+myName="jung_tae";
+console.log("my name is "+myName);
+

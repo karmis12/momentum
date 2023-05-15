6dbd43f31c2a60210abc908bff71123fe5c6f3ae
karmis12 <mjy1187@naver.com>
2023-05-15 16:37:13 +0900

    update knowledge

--- app.js
+++ app.js
@@ -11,3 +11,20 @@ console.log("my name is "+myName);
 myName="jung_tae";
 console.log("my name is "+myName);
 
+const theWeekend=["mon", "tue","wed","thu","fri","sat"];
+
+console.log(theWeekend);
+console.log(theWeekend[0]);
+theWeekend.push("sun");
+console.log(theWeekend);
+
+const jimmy={
+    age:25,
+    height:175,
+    weight:66,
+    blood:"b"
+}
+
+console.log(jimmy);
+console.log(jimmy.age);
+

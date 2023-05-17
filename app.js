309a0cdd24a28ce536faa521fef380e241e9321b
karmis12 <mjy1187@naver.com>
2023-05-17 22:15:27 +0900

    if, else if

--- app.js
+++ app.js
@@ -1,49 +1,14 @@
-// const calculator={
-//     plus:function(a,b){
-//         console.log(a+b);
-//     },
-//     minus:function(a,b){
-//         console.log(a-b);
-//     },
-//     multi:function(a,b){
-//         console.log(a*b);
-//     },
-//     divide:function(a,b){
-//         console.log(a/b);
-//     },
-//     square:function(a,b){
-//         console.log(a**b);
-//     }
-// }
+const age=parseInt(prompt("How old are you??"));
+// console.log(isNaN(age));
 
-// calculator.plus(9,3);
-// calculator.minus(9,3);
-// calculator.multi(9,3);
-// calculator.divide(9,3);
-// calculator.square(9,3);
-
-const calculator={
-    plus:function(a,b){
-        return a+b;
-    },
-    minus:function(a,b){
-        return a-b;
-    },
-    multi:function(a,b){
-        return a*b;
-    },
-    divide:function(a,b){
-        return a/b;
-    },
-    square:function(a,b){
-        return a**b;
-    }
+if(isNaN(age)||age<0){
+    console.log("Please tell your right age in number!!");
+}else if(age<20){
+    console.log("You are too young to drink alcohol");
+}else if(age>=20 && age<60){
+    console.log("Welcome What do you want to drink??");
+}else if(60<=age && age<=99){
+    console.log("Take care of your body!");
+}else{
+    console.log("Do whatever you want!");
 }
-
-const plusResult=calculator.plus(4,5);
-const minusResult=calculator.minus(plusResult,4);
-const multiResult=calculator.multi(7,minusResult);
-const divideResult=calculator.divide(multiResult,5);
-const squareResult=calculator.square(divideResult,2);
-
-console.log(plusResult, minusResult, multiResult, divideResult,squareResult);
--- homework/calculator.js
+++ homework/calculator.js
@@ -1,23 +1,49 @@
+// const calculator={
+//     plus:function(a,b){
+//         console.log(a,b);
+//     },
+//     minus:function(a,b){
+//         console.log(a,b);
+//     },
+//     multi:function(a,b){
+//         console.log(a,b);
+//     },
+//     divide:function(a,b){
+//         console.log(a,b);
+//     },
+//     square:function(a,b){
+//         console.log(a,b);
+//     }
+// }
+
+// calculator.add(9,3);
+// calculator.minus(9,3);
+// calculator.multi(9,3);
+// calculator.divide(9,3);
+// calculator.square(9,3);
+
 const calculator={
     plus:function(a,b){
-        console.log(a,b);
+        return a+b;
     },
     minus:function(a,b){
-        console.log(a,b);
+        return a-b;
     },
     multi:function(a,b){
-        console.log(a,b);
+        return a*b;
     },
     divide:function(a,b){
-        console.log(a,b);
+        return a/b;
     },
     square:function(a,b){
-        console.log(a,b);
+        return a**b;
     }
 }
 
-calculator.add(9,3);
-calculator.minus(9,3);
-calculator.multi(9,3);
-calculator.divide(9,3);
-calculator.square(9,3);
+const plusResult=calculator.plus(4,5);
+const minusResult=calculator.minus(plusResult,4);
+const multiResult=calculator.multi(7,minusResult);
+const divideResult=calculator.divide(multiResult,5);
+const squareResult=calculator.square(divideResult,2);
+
+console.log(plusResult, minusResult, multiResult, divideResult,squareResult);

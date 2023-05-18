f12d678bd83ede9d1ce3f12f28c011c817ce16b5
karmis12 <mjy1187@naver.com>
2023-05-18 15:50:26 +0900

    addEventListener

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const age=parseInt(prompt("How old are you??"));
-// console.log(isNaN(age));
+const hello= document.querySelector(".hello h1");
 
-if(isNaN(age)||age<0){
-    console.log("Please tell your right age in number!!");
-}else if(age<20){
-    console.log("You are too young to drink alcohol");
-}else if(age>=20 && age<60){
-    console.log("Welcome What do you want to drink??");
-}else if(60<=age && age<=99){
-    console.log("Take care of your body!");
-}else{
-    console.log("Do whatever you want!");
+function clickChecker(){
+    hello.style.color="blue";
 }
+
+hello.addEventListener("click",clickChecker);
--- test.js
+++ test.js
@@ -68,3 +68,21 @@ console.log(player.name)
 console.log(player.age)
 console.log(player.sports)
 player.sayHello("alice");
+
+// --- if문
+
+
+const age=parseInt(prompt("How old are you??"));
+// console.log(isNaN(age));
+
+if(isNaN(age)||age<0){
+    console.log("Please tell your right age in number!!");
+}else if(age<20){
+    console.log("You are too young to drink alcohol");
+}else if(age>=20 && age<60){
+    console.log("Welcome What do you want to drink??");
+}else if(60<=age && age<=99){
+    console.log("Take care of your body!");
+}else{
+    console.log("Do whatever you want!");
+}

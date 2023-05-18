ede4bb923b221dadd40ebfbe5b078727b1dfab38
karmis12 <mjy1187@naver.com>
2023-05-19 00:04:26 +0900

    toggle

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const h1= document.querySelector(".hello h1");
 
 function handleClick(){
-    const classClicked="clicked";
-    if (h1.classList.contains(classClicked)){
-        h1.classList.remove(classClicked);
-    }else{
-        h1.classList.add(classClicked);
-    }
+    // const classClicked="clicked";
+    // if (h1.classList.contains(classClicked)){
+    //     h1.classList.remove(classClicked);
+    // }else{
+    //     h1.classList.add(classClicked);
+    // }
+    // 위의 식이 아래의 식 하나로 대체됨
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click",handleClick);

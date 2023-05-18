891d222aa18b81ba33817af28669ff9307901c04
karmis12 <mjy1187@naver.com>
2023-05-18 23:29:56 +0900

    classlist

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
-const superEventHandler= document.querySelector(".hello h1");
-// console.dir(superEventHandler);
+const h1= document.querySelector(".hello h1");
 
-function handleMouseOn() {
-    superEventHandler.innerText = "The mouse is here!";
-    superEventHandler.style.color = "#1abc9c";
-  }
-  function handleMouseLeave() {
-    superEventHandler.innerText = "The mouse is gone!";
-    superEventHandler.style.color = "#3498db";
-  }
-  function handleWindowResize() {
-    superEventHandler.innerText = "You just resized!";
-    superEventHandler.style.color = "#9b59b6";
-  }
-  function handleMouseRightClick() {
-    superEventHandler.innerText = "That was a right click!";
-    superEventHandler.style.color = "#e74c3c";
-  }
-  function handleMouseClick() {
-    const currentColor = superEventHandler.style.color
-    let newColor; 
-    if (currentColor ==="blue"){
-        newColor="pink";
+function handleClick(){
+    const classClicked="clicked";
+    if (h1.classList.contains(classClicked)){
+        h1.classList.remove(classClicked);
     }else{
-        newColor= "blue";
+        h1.classList.add(classClicked);
     }
-    superEventHandler.style.color=newColor;
-    superEventHandler.innerText = "The title is clicked!";
-  }
-  superEventHandler.addEventListener("mouseenter", handleMouseOn);
-  superEventHandler.addEventListener("mouseleave", handleMouseLeave);
-  window.addEventListener("resize", handleWindowResize);
-  window.addEventListener("auxclick", handleMouseRightClick);
-  superEventHandler.addEventListener("click", handleMouseClick);
-  
+}
+
+h1.addEventListener("click",handleClick);

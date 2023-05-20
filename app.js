2d4ad1cd5b8b69d2c82a9e1a9603b8eac5ca731c
karmis12 <mjy1187@naver.com>
2023-05-20 23:19:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const link = document.querySelector("a");
 const greeting= document.querySelector(".greeting");
 
 // value는 input값 안에 유저가 치는 값을 말한다
-
-function onLoginSubmit(event){
-    event.preventDefault();
-    const username=loginInput.value;
-    loginForm.classList.add("hidden");
-    console.log(username);
-    // greeting.innerText="안녕하세요 "+username+"님";
-    greeting.innerText=`안녕하세요 ${username}님`;
-    greeting.classList.remove("hidden");
-}
-
 function handleLink(event){
     event.preventDefault();
     alert("clicked");
     // link.classList.add("hidden");
 }
+
+
+function handleLink(event){
+    event.preventDefault();
+    loginForm.classList.add("hidden");
+    greeting.classList.remove("hidden");
+    const username=loginInput.value;
+    greeting.innerText=`Hello ${username}`
+}
+
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLink);
